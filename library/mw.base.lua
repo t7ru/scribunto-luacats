f408b4a096ec23349cd0d018305fab2b420923dd
Benjamin <7@toru.ca>
2026-03-22 05:13:38 -0400

    feat: reference manual link for mw base

--- library/mw.base.lua
+++ library/mw.base.lua
@@ -6,55 +6,77 @@
 mw = {}
 
 --- Adds a warning which is displayed above the preview when previewing an edit.
+---
+--- [Reference manual](https://www.mediawiki.org/wiki/Extension:Scribunto/Lua_reference_manual#mw.addWarning)
 ---@param text string Parsed as wikitext.
 function mw.addWarning(text) end
 
 --- Calls `tostring()` on all arguments, then concatenates them with tabs as separators.
+---
+--- [Reference manual](https://www.mediawiki.org/wiki/Extension:Scribunto/Lua_reference_manual#mw.allToString)
 ---@param ... any
 ---@return string
 function mw.allToString(...) end
 
 --- Creates a deep copy of a value. All tables (and their metatables) are reconstructed from scratch. Functions are still shared, however.
+---
+--- [Reference manual](https://www.mediawiki.org/wiki/Extension:Scribunto/Lua_reference_manual#mw.clone)
 ---@generic T
 ---@param value T
 ---@return T
 function mw.clone(value) end
 
 --- Returns the current frame object, typically the frame object from the most recent `#invoke`.
+---
+--- [Reference manual](https://www.mediawiki.org/wiki/Extension:Scribunto/Lua_reference_manual#mw.getCurrentFrame)
 ---@return mw.frame
 function mw.getCurrentFrame() end
 
 --- Adds one to the "expensive parser function" count, and throws an exception if it exceeds the limit.
+---
+--- [Reference manual](https://www.mediawiki.org/wiki/Extension:Scribunto/Lua_reference_manual#mw.incrementExpensiveFunctionCount)
 function mw.incrementExpensiveFunctionCount() end
 
 --- Returns true if the current `#invoke` is being substed, false otherwise.
+---
+--- [Reference manual](https://www.mediawiki.org/wiki/Extension:Scribunto/Lua_reference_manual#mw.isSubsting)
 ---@return boolean
 function mw.isSubsting() end
 
 --- Efficiently loads large data tables. The loaded module is evaluated only once per page, rather than once per `#invoke` call.
 --- The returned table (and all subtables) may contain only booleans, numbers, strings, and other tables. Functions are not allowed.
+---
+--- [Reference manual](https://www.mediawiki.org/wiki/Extension:Scribunto/Lua_reference_manual#mw.loadData)
 ---@param module string The module to load (e.g., 'Module:Convert/data')
 ---@return table
 function mw.loadData(module) end
 
 --- This is the same as `mw.loadData()`, except it loads data from JSON pages rather than Lua tables.
 --- The JSON content must be an array or object.
+---
+--- [Reference manual](https://www.mediawiki.org/wiki/Extension:Scribunto/Lua_reference_manual#mw.loadJsonData)
 ---@param page string The JSON page to load.
 ---@return table
 function mw.loadJsonData(page) end
 
 --- Serializes object to a human-readable representation, then returns the resulting string.
+---
+--- [Reference manual](https://www.mediawiki.org/wiki/Extension:Scribunto/Lua_reference_manual#mw.dumpObject)
 ---@param object any
 ---@return string
 function mw.dumpObject(object) end
 
 --- Passes the arguments to `mw.allToString()`, then appends the resulting string to the log buffer.
 --- In the debug console, the function `print()` is an alias for this function.
+---
+--- [Reference manual](https://www.mediawiki.org/wiki/Extension:Scribunto/Lua_reference_manual#mw.log)
 ---@param ... any
 function mw.log(...) end
 
 --- Calls `mw.dumpObject()` and appends the resulting string to the log buffer.
 --- If prefix is given, the logged text will be "prefix = object-string".
+---
+--- [Reference manual](https://www.mediawiki.org/wiki/Extension:Scribunto/Lua_reference_manual#mw.logObject)
 ---@param object any
 ---@param prefix? string
 function mw.logObject(object, prefix) end
--- library/mw.hash.lua
+++ library/mw.hash.lua
@@ -6,6 +6,8 @@ mw.hash = {}
 
 --- Hashes a string value with the specified algorithm.
 --- Valid algorithms may be fetched using `mw.hash.listAlgorithms()`.
+---
+--- [Reference manual](https://www.mediawiki.org/wiki/Extension:Scribunto/Lua_reference_manual#mw.hash.hashValue)
 ---@param algo string
 ---@param value string
 ---@return string
@@ -13,5 +15,7 @@ function mw.hash.hashValue(algo, value) end
 
 --- Returns a list of supported hashing algorithms.
 --- This method is a passthrough of PHP's `hash_algos()`.
+---
+--- [Reference manual](https://www.mediawiki.org/wiki/Extension:Scribunto/Lua_reference_manual#mw.hash.listAlgorithms)
 ---@return table<integer, string>
 function mw.hash.listAlgorithms() end
